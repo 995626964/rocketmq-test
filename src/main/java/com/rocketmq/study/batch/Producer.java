ba130fe8bd355ef781a44495fd49b7707f243c1d
995626964 <995626964@qq.com>
2021-07-12 16:11:29 +0800

    批量消息

--- src/main/java/com/rocketmq/study/batch/Producer.java
+++ src/main/java/com/rocketmq/study/batch/Producer.java
@@ -1,4 +1,43 @@
 package com.rocketmq.study.batch;
 
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Producer {
+    public static void main(String[] args) throws MQClientException, MQBrokerException, RemotingException, InterruptedException {
+        //1.创建消息生产者producer,并制定生产者组名
+        DefaultMQProducer defaultMQProducer = new DefaultMQProducer("group1");
+        //2.指定NameServer地址
+        defaultMQProducer.setNamesrvAddr("1.15.235.58:9876;8.140.25.64:9876");
+        //3.启动producer
+        defaultMQProducer.start();
+        //4.创建消息对象，指定主题Topic,Tag和消息体
+        /**
+         * 参数一：消息主题Topic
+         * 参数二：消息Tag
+         * 参数三：消息内容
+         */
+        Message msg1 = new Message("BatchTopic", "Tag1", ("Hello World" + 1).getBytes());
+        Message msg2 = new Message("BatchTopic", "Tag1", ("Hello World" + 2).getBytes());
+        Message msg3 = new Message("BatchTopic", "Tag1", ("Hello World" + 3).getBytes());
+        List<Message> msgs=new ArrayList<>();
+        msgs.add(msg1);
+        msgs.add(msg2);
+        msgs.add(msg3);
+
+        //5.发送消息
+        SendResult result = defaultMQProducer.send(msgs);
+        System.out.println(result.toString());
+        Thread.sleep(1000);
+        //6.关闭生产者producer
+        defaultMQProducer.shutdown();
+    }
+
 }
